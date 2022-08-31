c6ce0265289d96f68a8c23dd1d85c64f31e15e00
shubham1020k <gyaninfo128@gmail.com>
2022-08-31 08:32:52 +0530

    decoration of MealItem

--- lib/Widget/mealItem.dart
+++ lib/Widget/mealItem.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../Models/meal.dart';
 
-
 class MealItem extends StatelessWidget {
   final String title;
   final String imageUrl;
@@ -9,12 +8,48 @@ class MealItem extends StatelessWidget {
   final Complexity complexity;
   final Affordability affordability;
 
-   const MealItem(
-      {Key key, @required this.title,
+  const MealItem(
+      {Key key,
+      @required this.title,
       @required this.imageUrl,
       @required this.duration,
       @required this.complexity,
-      @required this.affordability}) : super(key: key);
+      @required this.affordability})
+      : super(key: key);
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get AffordabilityText {
+    switch (affordability) {
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'Unknow';
+    }
+  }
 
   void selectMealItem() {}
 
@@ -28,26 +63,81 @@ class MealItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
         ),
         margin: const EdgeInsets.all(10),
-        child: Column(children: <Widget>[
-          Stack(
-            children: <Widget>[
-              ClipRRect(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
                   borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(15),
                     topRight: Radius.circular(15),
                   ),
-                  child: Image.network(imageUrl,height:250 ,width: double.infinity,
-                  fit: BoxFit.cover,),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
                   ),
-            ],
-           
-          )
-        ],
-      
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      title,
+                      style: TextStyle(color: Colors.white, fontSize: 26),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                   Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        '$duration min', //style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(AffordabilityText),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
-
-
     );
   }
 }
